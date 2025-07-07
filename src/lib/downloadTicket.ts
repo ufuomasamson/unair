fe5d8ce417419b084a79e2b8479b3d2c58048fba
ufuomasamson <samsonufuomaenzo@gmail.com>
2025-07-07 07:08:25 -0700

    fix: remove duplicate FlightTicket

--- src/lib/downloadTicket.ts
+++ src/lib/downloadTicket.ts
@@ -2,7 +2,11 @@ import html2pdf from 'html2pdf.js';
 
 export function downloadTicket() {
   const ticket = document.getElementById('ticket');
-  if (!ticket) return;
+  if (!ticket) {
+    alert('Ticket element not found!');
+    return;
+  }
+  console.log('Ticket element found:', ticket);
   html2pdf()
     .set({
       margin: 0.5,
